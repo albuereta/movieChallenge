3a16c638776eb810f5bbba6f8f7778aea387dc95
Francesc Albuera <francesc.albuera@gmail.com>
2022-10-10 10:41:12 +0200

    add interceptor to show spinner while we wait for api response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,10 +10,12 @@ import { MovieDetailComponent } from './components/movie-detail/movie-detail.com
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { TvShowDetailComponent } from './components/tv-show-detail/tv-show-detail.component';
 import { TvShowsListComponent } from './components/tv-shows-list/tv-shows-list.component';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
+import { CardDetailComponent } from './shared/components/card-detail/card-detail.component';
 import { CardComponent } from './shared/components/card/card.component';
 import { CarouselComponent } from './shared/components/carousel/carousel.component';
 import { ProgressBarComponent } from './shared/components/progress-bar/progress-bar.component';
-import { CardDetailComponent } from './shared/components/card-detail/card-detail.component';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,9 +28,12 @@ import { CardDetailComponent } from './shared/components/card-detail/card-detail
     ProgressBarComponent,
     HeaderComponent,
     CardDetailComponent,
+    SpinnerComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/interceptors/loader.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { LoaderService } from '../services/loader.service';
+
+@Injectable()
+export class LoaderInterceptor implements HttpInterceptor {
+  constructor(private loaderService: LoaderService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    this.loaderService.setLoading(true);
+    return next.handle(request).pipe(
+      finalize(() => {
+        this.loaderService.setLoading(false);
+      })
+    );
+  }
+}
